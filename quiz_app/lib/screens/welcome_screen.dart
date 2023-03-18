349751bfa7bb93a2dafa903c6eba55206c6f7adc
kingjedidiah01 <akinwahnde20@gmail.com>
2023-03-18 14:19:05 +0100

    welcome screen update
    4th commit

--- quiz_app/lib/screens/welcome_screen.dart
+++ quiz_app/lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
+import '../constants.dart';
+
 // ignore: camel_case_types
 class welcomeScreen extends StatelessWidget {
   const welcomeScreen({super.key});
@@ -8,15 +10,48 @@ class welcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        WebsafeSvg.asset("assets/icons/bg.svg"),
-        SafeArea(
-          child: Column(children: [
-            Text("Let's play Quiz,",
-                style: Theme.of(context).textTheme.headlineLarge),
-          ]),
-        )
-      ]),
+      body: Stack(
+        children: [
+          WebsafeSvg.asset("assets/icons/bg.svg"),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                ),
+                const Spacer(),
+                Text(
+                  "Let's play Quiz,",
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                const Text("Enter your information below"),
+                const Spacer(),
+                const TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0x771c2341),
+                    hintText: "full Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.all(
+                      kDefaultPadding * 0.75), //equals to 15
+                  decoration: const BoxDecoration(gradient: kPrimaryGradient),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
